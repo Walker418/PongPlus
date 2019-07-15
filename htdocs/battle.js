6a8f8ee33740f9e345e1e8ca8e863e6d8bfae68a
Walker418 <walker418@hotmail.com>
2019-07-15 16:27:40 +0900

    ゲームプレイ部分仮実装

--- htdocs/battle.js
+++ htdocs/battle.js
@@ -15,10 +15,10 @@ const io   = require("socket.io")(http);
 // Webサーバ
 //--------------------------------------
 app.get("/", (req, res)=>{
-  res.sendFile(__dirname + "/charmove_3.html");
+  res.sendFile(__dirname + "/battle.html");
 });
-app.get("/image/:file", (req, res)=>{
-  res.sendFile(__dirname + "/image/" + req.params.file);
+app.get("/assets/image/:file", (req, res)=>{
+  res.sendFile(__dirname + "/assets/image/" + req.params.file);
 });
 http.listen(port, ()=>{
   console.log(`listening on *:${port}`);
@@ -28,12 +28,17 @@ http.listen(port, ()=>{
 // Socket.io
 //--------------------------------------
 // 定数
-const MAX_WIDTH  = 400;     // 横幅
-const MAX_HEIGHT = 300;     // 高さ
+const MAX_WIDTH  = 600;     // 横幅
+const MAX_HEIGHT = 400;     // 高さ
+
+const RacketSizeX = 20;     //ラケットの横幅
+const RacketSizeY = 100;    //ラケットの高さ
 
 // プレイヤー情報 (マスター)
 let CHAR_LIST = {};
 let ZINDEX = 1000;          //レイヤーの順番
+
+let Boll = {};
 
 io.on("connection", (socket)=>{
   console.log(`ユーザー「${socket.id}」が接続しました`);
@@ -66,6 +71,7 @@ io.on("connection", (socket)=>{
     ZINDEX++;
 
     console.log("[login]", CHAR_LIST);
+    addBoll();
   });
 
   //----------------------------
@@ -81,6 +87,20 @@ io.on("connection", (socket)=>{
     // 全ユーザーへ送信
     io.emit("movechar", data);
     console.log("[movechar]", data);
+  });
+
+  //----------------------------
+  // ボールの動きを同期
+  //----------------------------
+  socket.on("moveBoll", (data)=>{
+    // サーバ内で座標を計算
+    moveBoll(data.token);
+
+    // 計算後の座標をセット
+    data.pos = Boll[data.token].pos;
+
+    // 全ユーザーへ送信
+    io.emit("moveBoll", data);
   });
 
   //----------------------------
@@ -162,4 +182,80 @@ function removeChar(id){
 
   console.log("[removeChar]", "NotFound", id);
   return(false);
+}
+
+/**
+ * ラケットの当たり判定
+ *
+ * @param {string}  token  プレイヤー識別用の文字列
+ */
+function collisionRacket(token){
+    const pos = CHAR_LIST[token].pos;
+}
+
+/**
+ *　ボールの生成
+ */
+function addBoll()
+{
+  
+  let len = 32;
+  const ls = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let result = "";
+
+  for(let i = 0; i < len; i++){
+    const idx = Math.floor( Math.random() * ls.length );
+    result += ls[idx];
+  }
+
+  // 初期座標を決定
+  const pos = {
+    x: 300,
+    y: 200
+  };
+  const size = {
+    x: 20,
+    y: 22000
+  };
+  const step = {
+    x: 10,
+    y: 10
+  };
+
+
+  Boll[result] = {
+       pos: pos,          // 座標
+    zindex: 2000,       // レイヤー深度
+    size:size,
+    step:step,
+  };
+
+  // ユーザーに通知
+  io.emit("addBoll", {token:result,  pos:pos, zindex:2000, id:result});
+
+  console.log("[add]", Boll);
+}
+
+function moveBoll(token)
+{
+  const pos = Boll[token].pos;    // 現在の座標を取得
+  const step = Boll[token].step;
+  let x, y;
+
+      x = pos.x + step.x;
+      y = pos.y + step.y;
+
+
+    // マスター更新
+    Boll[token].pos.x = x;
+    Boll[token].pos.y = y;
+}
+
+/**
+ *　ボールの判定
+ *
+ *
+ */
+function consollisionBoll(){
+
 }
