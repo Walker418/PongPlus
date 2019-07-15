cbc5aabc15e887f485e60240b36950fa3ec7d5a1
Walker418 <walker418@hotmail.com>
2019-07-15 12:06:53 +0900

    とりあえず上げ

--- server.js
+++ server.js
@@ -19,14 +19,24 @@ const io   = require("socket.io")(http);
 //--------------------------------------
 // Webサーバ
 //--------------------------------------
+
 app.get("/", (req, res)=>{
-  res.sendFile(DOCUMENT_ROOT + "index.html");		// タイトル画面
+	res.sendFile(DOCUMENT_ROOT + "index.html");		// タイトル画面
 });
 app.get("/matching", (req, res)=>{
 	res.sendFile(DOCUMENT_ROOT + "matching.html");	// マッチング画面
-})
+});
+app.get("/battle", (req, res)=>{
+	res.sendFile(DOCUMENT_ROOT + "battle.html");	// 対戦画面
+});
+app.get("/result", (req, res)=>{
+	res.sendFile(DOCUMENT_ROOT + "result.html");	// リザルト画面
+});
+app.get("/assets/image/:file", (req, res)=>{				// 画像ファイル
+	res.sendFile(DOCUMENT_ROOT + "/assets/image/" + req.params.file);
+ });
 http.listen(port, ()=>{
-  console.log(`listening on *:${port}`);
+	console.log(`listening on *:${port}`);
 });
 
 //--------------------------------------
@@ -38,4 +48,4 @@ const MAX_HEIGHT = 300;     // 高さ
 
 // プレイヤー情報 (マスター)
 let CHAR_LIST = {};
-let ZINDEX = 1000;          //レイヤーの順番
+let ZINDEX = 1000;          // レイヤーの順番
